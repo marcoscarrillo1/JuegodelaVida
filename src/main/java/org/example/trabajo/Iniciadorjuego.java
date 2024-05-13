210433b92eb5daabf06465bf86fb4a5c042a9d8f
gabrielcarballosa <gabriel.carballosa@edu.uah.es>
2024-05-13 16:22:07 +0200

    KLK

--- src/main/java/org/example/trabajo/Iniciadorjuego.java
+++ src/main/java/org/example/trabajo/Iniciadorjuego.java
@@ -1,17 +1,26 @@
 package org.example.trabajo;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Iniciadorjuego extends Application {
     //private Stage primaryStage;
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Iniciadorjuego.class.getResource("Pantallainicial.fxml")); // Va a cargar un fichero .fxml
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Juego de la vida");
+        stage.setScene(scene);
+        stage.show();
     }
 
-
+    public static void main(String[] args) {
+        launch();
+    }
 }
+
+
+
---
+++ src/main/java/org/example/trabajo/InicializadorControler.java
@@ -0,0 +1,17 @@
+package org.example.trabajo;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class InicializadorControler {
+    @FXML
+    protected void cargarPartidaClick(){
+
+    }
+    @FXML
+    protected void nuevaPartidaClick(){
+
+
+    }
+
+}
